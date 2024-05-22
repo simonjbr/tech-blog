44ec8f3b1bba0ce07bc7c4bd39dd9ea89f36db03
Simon Brown <simonandon@live.com.au>
2024-05-22 09:59:39 +0800

    Wrote basic route for homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { User, Blog } = require('../models');
+
+// get all blogs for homepage
+router.get('/', async (req, res) => {
+	try {
+		const blogs = await Blog.findAll({
+			raw: true,
+			include: User,
+		});
+
+		res.status(200).render('homepage', {
+			blogs,
+		});
+
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+module.exports = router;
