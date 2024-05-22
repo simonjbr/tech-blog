1c3973d1b8075596ee1e53a35161dc707291d03b
Simon Brown <simonandon@live.com.au>
2024-05-22 12:14:08 +0800

    Set up associations for User/Blog and Comment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import models so we can set up their relationships
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 // set up one-to-many relationship between User and Blog
 User.hasMany(Blog, {
@@ -10,6 +11,26 @@ User.hasMany(Blog, {
 
 Blog.belongsTo(User, {
 	foreignKey: 'user_id',
+});
+
+// set up one-to-many relationship between User and Comment
+User.hasMany(Comment, {
+	foreignKey: 'user_id',
+	onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+	foreignKey: 'user_id',
+});
+
+// set up one-to-many relationship between Blog and Comment
+Blog.hasMany(Comment, {
+	foreignKey: 'blog_id',
+	onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Blog, {
+	foreignKey: 'blog_id',
 });
 
 module.exports = {
