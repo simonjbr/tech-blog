6b2f998e9fd6a7f56f8dc442d04f283404f4633c
Simon Brown <simonandon@live.com.au>
2024-05-23 09:46:09 +0800

    Now passing loggedIn variable to render calls. And redirecting from /login and /signup if already logged in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,6 +16,7 @@ router.get('/', async (req, res) => {
 
 		res.status(200).render('homepage', {
 			blogs,
+			loggedIn: req.session.logged_in,
 		});
 
 	} catch (error) {
@@ -26,7 +27,16 @@ router.get('/', async (req, res) => {
 // signup page
 router.get('/signup', async (req, res) => {
 	try {
-		res.status(200).render('signup');
+		// if logged in redirect to homepage
+		if (req.session.logged_in) {
+			res.redirect('/');
+			return;
+		}
+
+		// otherwise render signup page
+		res.status(200).render('signup', {
+			loggedIn: req.session.logged_in,
+		});
 	} catch (error) {
 		res.status(500).json(error);
 	}
@@ -35,7 +45,32 @@ router.get('/signup', async (req, res) => {
 // login page
 router.get('/login', async (req, res) => {
 	try {
-		res.status(200).render('login');
+		// if logged in redirect to homepage
+		if (req.session.logged_in) {
+			res.redirect('/');
+			return;
+		}
+
+		// otherwise render login page
+		res.status(200).render('login', {
+			loggedIn: req.session.logged_in,
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+// logout route
+router.get('/logout', async (req, res) => {
+	try {
+		// if logged in kill session
+		if (req.session.logged_in) {
+			req.session.destroy(() => {
+				res.status(204).redirect('/');
+			});
+		} else {
+			res.status(404).end();
+		}
 	} catch (error) {
 		res.status(500).json(error);
 	}
