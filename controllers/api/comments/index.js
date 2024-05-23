c1eb588e18e8ff8ec482798c842ab50581d8ef1f
Simon Brown <simonandon@live.com.au>
2024-05-23 11:44:48 +0800

    Created route for POSTing new comments

---
+++ controllers/api/comments/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { User, Blog, Comment } = require('../../../models');
+
+// import custom middleware
+const isLoggedIn = require('../../../utils/auth');
+
+// /api/comments POST new comment route
+router.post('/', isLoggedIn, async (req, res) => {
+	try {
+		const newComment = await Comment.create({
+			content: req.body.content,
+			blog_id: req.body.blog_id,
+			user_id: req.session.user_id,
+		});
+
+		console.log(newComment);
+
+		res.status(200).json(newComment);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./users');
 const blogRoutes = require('./blogs');
+const commentRoutes = require('./comments');
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
