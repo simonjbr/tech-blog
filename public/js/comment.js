13476ce1ccf4965aaf41b7b94c8862f4e9f08a47
Simon Brown <simonandon@live.com.au>
2024-05-23 11:40:32 +0800

    Created event handler for new comment form submit in public/js/comment.js

---
+++ public/js/comment.js
@@ -0,0 +1,39 @@
+const addCommentHandler = async (event) => {
+	// prevent default form behaviour
+	event.preventDefault();
+
+	// get reference to comment input
+	const commentInput = document.querySelector('#comment-input');
+
+	// extract content and id data
+	const content = commentInput.value;
+	const user_id = commentInput.dataset.userId;
+	const blog_id = commentInput.dataset.blogId;
+
+	// form validation
+	if (content) {
+		// send fetch POST request to /api/comments
+		const response = await fetch('/api/comments', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				content,
+				user_id,
+				blog_id
+			}),
+		});
+
+		if (response.ok) {
+			document.location.reload();
+		} else {
+			alert('Failed to post new comment.');
+		}
+	}
+
+};
+
+document
+	.querySelector('#comment-form')
+	.addEventListener('submit', addCommentHandler);
