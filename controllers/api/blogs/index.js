2eb3522a8eaaffffc1ffb747636058023ecab7b7
Simon Brown <simonandon@live.com.au>
2024-05-25 09:34:39 +0800

    Created route to render edit blog page

--- controllers/api/blogs/index.js
+++ controllers/api/blogs/index.js
@@ -4,6 +4,37 @@ const { User, Blog, Comment } = require('../../../models');
 // import custom middleware
 const isLoggedIn = require('../../../utils/auth');
 
+// /api/blogs/edit/:id to render edit blog page
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
+	try {
+		const blogId = req.params.id;
+		const dbBlogData = await Blog.findByPk(blogId, {
+			include: [
+				{
+					model: User,
+				},
+				{
+					model: Comment,
+					include: User
+				}
+			],
+		});
+
+		// serialize data for rendering
+		const blog = dbBlogData.get({ plain: true });
+
+		// render edit blog page
+		res.render('edit-blog', {
+			blog,
+			user_id: req.session.user_id,
+			loggedIn: req.session.logged_in,
+		});
+
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
 // /api/blogs/:id route for individual blog page
 router.get('/:id', async (req, res) => {
 	try {
@@ -23,11 +54,12 @@ router.get('/:id', async (req, res) => {
 		// serialize data for rendering
 		const blog = dbBlogData.get({ plain: true });
 
+		// render individual blog page
 		res.render('blog', {
 			blog,
 			user_id: req.session.user_id,
 			loggedIn: req.session.logged_in,
-		})
+		});
 	} catch (error) {
 		res.status(500).json(error);
 	}
